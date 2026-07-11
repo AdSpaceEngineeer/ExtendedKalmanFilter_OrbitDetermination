b49f39f4ca9bf6025119ef9cdce256dd6ac0208d
AdSpaceEngineer <88097446+AdSpaceEngineeer@users.noreply.github.com>
2026-07-11 13:45:21 +0800

    Remove duplicate src/ekf_od/src/ekf_od/__init__.py

--- src/ekf_od/src/ekf_od/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Extended Kalman Filter orbit-determination demo package."""
-
-from .filter import ExtendedKalmanFilter
-from .simulation import DemoResult, run_demo
-
-__all__ = ["DemoResult", "ExtendedKalmanFilter", "run_demo"]
