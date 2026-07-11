2ce74747985f221da88d4968387df856a3ff6389
AdSpaceEngineer <88097446+AdSpaceEngineeer@users.noreply.github.com>
2026-07-11 14:02:43 +0800

    Add GaussIODResult and estimate_gauss_states to __all__

--- src/ekf_od/__init__.py
+++ src/ekf_od/__init__.py
@@ -1,6 +1,7 @@
 """Extended Kalman Filter orbit-determination demo package."""
 
+from .angles import GaussIODResult, estimate_gauss_states
 from .filter import ExtendedKalmanFilter
 from .simulation import DemoResult, run_demo
 
-__all__ = ["DemoResult", "ExtendedKalmanFilter", "run_demo"]
+__all__ = ["DemoResult", "ExtendedKalmanFilter", "GaussIODResult", "estimate_gauss_states", "run_demo"]
